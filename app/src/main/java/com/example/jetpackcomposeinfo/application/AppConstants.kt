dca4e00e3fcf92059f9f335868de37ff1bb1006b
Fernando Serra <fernando.serra.r@gmail.com>
2021-08-26 20:16:19 -0400

    Update WebService

--- app/src/main/java/com/example/jetpackcomposeinfo/application/AppConstants.kt
+++ app/src/main/java/com/example/jetpackcomposeinfo/application/AppConstants.kt
@@ -1,4 +1,6 @@
 package com.example.jetpackcomposeinfo.application
 
 object AppConstants {
+
+    const val BASE_URL = "https://www.balldontlie.io/api/v1/" //funcionando
 }
--- app/src/main/java/com/example/jetpackcomposeinfo/data/remote/WebService.kt
+++ app/src/main/java/com/example/jetpackcomposeinfo/data/remote/WebService.kt
@@ -1,4 +1,17 @@
 package com.example.jetpackcomposeinfo.data.remote
 
+import com.example.jetpackcomposeinfo.data.model.ReqNBA
+import com.example.jetpackcomposeinfo.data.model.Team
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface WebService {
+
+    @GET("teams")
+    suspend fun getTeams() : ReqNBA
+
+    @GET("teams/{id}")
+    suspend fun getTeam(@Path("id") id: Int) : Team
+
+
 }
