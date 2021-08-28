6ab668c176cf7985cc5e0d5dd39a438c698f250f
Fernando Serra <fernando.serra.r@gmail.com>
2021-08-27 23:59:47 -0400

    Create LazyColumn

--- app/src/main/java/com/example/jetpackcomposeinfo/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeinfo/MainActivity.kt
@@ -2,21 +2,25 @@ package com.example.jetpackcomposeinfo
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetpackcomposeinfo.data.model.Team
 import com.example.jetpackcomposeinfo.data.remote.DataSourceImpl
 import com.example.jetpackcomposeinfo.domain.RepositoryImpl
 import com.example.jetpackcomposeinfo.presentation.DataVMFactory
 import com.example.jetpackcomposeinfo.presentation.DataViewModel
+import com.example.jetpackcomposeinfo.ui.home.TeamCard
 import com.example.jetpackcomposeinfo.ui.theme.JetpackComposeInfoTheme
 import com.example.jetpackcomposeinfo.utils.Resource
 import kotlin.math.log
@@ -54,9 +58,23 @@ fun ShowList(viewModel: DataViewModel){
         }
         is Resource.Success ->{
             Log.i("MainActiviy", "ShowList:  ${(result as Resource.Success<List<Team>>).data.size}")
+            RvTeams(team= (result as Resource.Success<List<Team>>).data)
         }
     }
 
+}
+
+@Composable
+fun RvTeams(team:List<Team>){
+    val context = LocalContext.current
+    LazyColumn{
+        items(team.size) { dat->
+            TeamCard(team = team[dat], onDatosClick = {
+                Toast.makeText(context, "Equipo ${it}", Toast.LENGTH_LONG)
+                    .show()
+            })
+        }
+    }
 }
 
 @Composable
--- app/src/main/java/com/example/jetpackcomposeinfo/ui/home/HomeComponents.kt
+++ app/src/main/java/com/example/jetpackcomposeinfo/ui/home/HomeComponents.kt
@@ -1,8 +1,42 @@
 package com.example.jetpackcomposeinfo.ui.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.jetpackcomposeinfo.data.model.Team
+import com.example.jetpackcomposeinfo.R
 
 @Composable
-fun TeamCard(){
+fun TeamCard(team:Team, onDatosClick: (Team) -> Unit){
+    Surface(shape = RoundedCornerShape(8.dp), elevation = 8.dp,modifier = Modifier.padding(8.dp)  ) {
+        Column(modifier = Modifier.padding(16.dp).clickable (onClick = { onDatosClick(team) } )) {
+            val imageModifier= Modifier
+                .height(150.dp)
+                .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(8.dp))
 
+            Image( painterResource(R.drawable.img) ,  contentDescription = "",modifier = imageModifier,contentScale = ContentScale.Crop)
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = team.full_name, fontSize = 30.sp, style = MaterialTheme.typography.h2)
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun PreviewTeamCard(){
+    TeamCard(team = Team(1,"Lk","NY","Est","mm","Angeles Lakers","Lakers"),onDatosClick = {})
 }
