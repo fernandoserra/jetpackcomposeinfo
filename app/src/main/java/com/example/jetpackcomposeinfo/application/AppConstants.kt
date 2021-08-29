0d7807e7a1360eff29a55db4b833ca68c7441b5b
Fernando Serra <fernando.serra.r@gmail.com>
2021-08-29 01:10:44 -0400

    Add library coil  Update TeamCard

--- app/src/main/java/com/example/jetpackcomposeinfo/application/AppConstants.kt
+++ app/src/main/java/com/example/jetpackcomposeinfo/application/AppConstants.kt
@@ -3,4 +3,5 @@ package com.example.jetpackcomposeinfo.application
 object AppConstants {
 
     const val BASE_URL = "https://www.balldontlie.io/api/v1/" //funcionando
+    const val BASE_IMG ="https://firebasestorage.googleapis.com/v0/b/nba-app-8f173.appspot.com/o/teams%2F"
 }
--- app/src/main/java/com/example/jetpackcomposeinfo/ui/home/HomeComponents.kt
+++ app/src/main/java/com/example/jetpackcomposeinfo/ui/home/HomeComponents.kt
@@ -3,40 +3,105 @@ package com.example.jetpackcomposeinfo.ui.home
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import coil.ImageLoader
+import coil.compose.LocalImageLoader
+import coil.compose.rememberImagePainter
+import coil.decode.SvgDecoder
 import com.example.jetpackcomposeinfo.data.model.Team
-import com.example.jetpackcomposeinfo.R
+import com.example.jetpackcomposeinfo.application.AppConstants.BASE_IMG
 
-@Composable
-fun TeamCard(team:Team, onDatosClick: (Team) -> Unit){
-    Surface(shape = RoundedCornerShape(8.dp), elevation = 8.dp,modifier = Modifier.padding(8.dp)  ) {
-        Column(modifier = Modifier.padding(16.dp).clickable (onClick = { onDatosClick(team) } )) {
-            val imageModifier= Modifier
+/*@Composable
+fun TeamCard(team: Team, onDatosClick: (Team) -> Unit) {
+    Surface(shape = RoundedCornerShape(8.dp), elevation = 8.dp, modifier = Modifier.padding(8.dp)) {
+        Column(modifier = Modifier
+            .padding(16.dp)
+            .clickable(onClick = { onDatosClick(team) })) {
+            val imageModifier = Modifier
                 .height(150.dp)
                 .fillMaxWidth()
                 .clip(shape = RoundedCornerShape(8.dp))
 
-            Image( painterResource(R.drawable.img) ,  contentDescription = "",modifier = imageModifier,contentScale = ContentScale.Crop)
+            Image(
+                painterResource(R.drawable.img),
+                contentDescription = "",
+                modifier = imageModifier,
+                contentScale = ContentScale.Crop
+            )
             Spacer(modifier = Modifier.height(16.dp))
             Text(text = team.full_name, fontSize = 30.sp, style = MaterialTheme.typography.h2)
         }
     }
+}*/
+
+@Composable
+fun TeamCard(team: Team, onDatosClick: (Team) -> Unit) {
+
+    val imageLoader = ImageLoader.Builder(LocalContext.current)
+        .componentRegistry {
+            add(SvgDecoder(LocalContext.current))
+        }
+        .availableMemoryPercentage(0.25)
+        .crossfade(true)
+        .build()
+
+    Surface(shape = RoundedCornerShape(8.dp), elevation = 8.dp,modifier = Modifier.padding(8.dp)) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(4.dp))
+                .clickable(onClick = { onDatosClick(team) })
+                .padding(16.dp),
+
+            ) {
+            Surface(
+                modifier = Modifier.size(80.dp),
+                shape = CircleShape//,
+                //color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+            ) {
+
+                val urlSvg = "${BASE_IMG}${team.id}.svg?alt=media"
+                CompositionLocalProvider(LocalImageLoader provides imageLoader) {
+                    val painter = rememberImagePainter(urlSvg)
+                    Image(
+                        painter = painter,
+                        contentDescription = "",
+                        contentScale = ContentScale.Crop
+                    )
+                }
+            }
+            Column(
+                modifier = Modifier
+                    .padding(start = 8.dp)
+                    .align(Alignment.Top)
+            )
+            {
+                Text(text = team.full_name,  style = MaterialTheme.typography.h5)
+                Text(text = team.city, style = MaterialTheme.typography.body2)
+            }
+        }
+    }
+
 }
 
 
-@Preview
+@Preview(showBackground = true)
 @Composable
-fun PreviewTeamCard(){
-    TeamCard(team = Team(1,"Lk","NY","Est","mm","Angeles Lakers","Lakers"),onDatosClick = {})
+fun PreviewTeamCard() {
+    TeamCard(
+        team = Team(1, "Lk", "California", "Est", "mm", "Angeles Lakers", "Lakers"),
+        onDatosClick = {})
 }
