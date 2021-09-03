16aa64ae40be59ffcbb72a42da660aea0c0791b9
Fernando Serra <fernando.serra.r@gmail.com>
2021-09-03 01:10:39 -0400

    Update Package Data (DataSource)

--- app/src/main/java/com/example/jetpackcomposeinfo/data/remote/DataSource.kt
+++ app/src/main/java/com/example/jetpackcomposeinfo/data/remote/DataSource.kt
@@ -1,12 +1,15 @@
 package com.example.jetpackcomposeinfo.data.remote
 
+import com.example.jetpackcomposeinfo.data.model.ReqGamesNBA
 import com.example.jetpackcomposeinfo.data.model.ReqNBA
 import com.example.jetpackcomposeinfo.data.model.Team
 import com.example.jetpackcomposeinfo.utils.Resource
+import retrofit2.http.Path
 
 interface DataSource {
 
     suspend fun getTeams(): Resource<List<Team>>
     suspend fun getTeam(id: Int) : Resource<Team>
+    suspend fun getGamesTeam(seasons: Int, id: Int): Resource<ReqGamesNBA>
 
 }
--- app/src/main/java/com/example/jetpackcomposeinfo/data/remote/DataSourceImpl.kt
+++ app/src/main/java/com/example/jetpackcomposeinfo/data/remote/DataSourceImpl.kt
@@ -1,6 +1,6 @@
 package com.example.jetpackcomposeinfo.data.remote
 
-import com.example.jetpackcomposeinfo.data.model.ReqNBA
+import com.example.jetpackcomposeinfo.data.model.ReqGamesNBA
 import com.example.jetpackcomposeinfo.data.model.Team
 import com.example.jetpackcomposeinfo.utils.Resource
 
@@ -11,5 +11,9 @@ class DataSourceImpl:DataSource {
 
     override suspend fun getTeam(id: Int): Resource<Team> {
         return  Resource.Success(RetrofitClient.webService.getTeam(id))
+    }
+
+    override suspend fun getGamesTeam(seasons: Int, id: Int): Resource<ReqGamesNBA> {
+        return  Resource.Success(RetrofitClient.webService.getGamesTeam(seasons,id))
     }
 }
