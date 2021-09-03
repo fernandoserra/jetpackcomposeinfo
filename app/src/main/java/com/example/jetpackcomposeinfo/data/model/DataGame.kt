4e5fca4ce18ab92f40062258ef2c8830cbff5df5
Fernando Serra <fernando.serra.r@gmail.com>
2021-09-03 18:10:39 -0400

    Update model DataGame

--- app/src/main/java/com/example/jetpackcomposeinfo/data/model/DataGame.kt
+++ app/src/main/java/com/example/jetpackcomposeinfo/data/model/DataGame.kt
@@ -3,13 +3,13 @@ package com.example.jetpackcomposeinfo.data.model
 data class DataGame(
     val id: Long,
     val date: String,
-    val homeTeam: Team,
-    val homeTeamScore: Long,
+    val home_team: Team,
+    val home_team_score: Int,
     val period: Long,
     val postseason: Boolean,
     val season: Long,
     val status: String,
     val time: String,
-    val visitorTeam: Team,
-    val visitorTeamScore: Long
+    val visitor_team: Team,
+    val visitor_team_score: Int
 )
